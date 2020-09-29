e95b8dc15274b2c4142441ab378457e498b5996d
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-29 00:16:11 -0700

    append city stats

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+$(document).ready(function () {
 
-
+    
+    
+    
     $("#searchBtn").on("click", function(event) {
 
     var city_name = $("#citySearchBox").val();
+    // var cityTemp = $(".temp").val();
+    // var cityHum = $(".humidity").val();
+    // var cityWind = $(".windSpeed").val();
     
+
     console.log(city_name)
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=37574b1d6986c3e876db8a1d6e2f05dc";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&units=imperial&appid=37574b1d6986c3e876db8a1d6e2f05dc";
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -15,15 +22,23 @@
 
         
 
+        $(".cityName").append(city_name);
+        $(".temp").append("Temperature: ", response.main.temp.toFixed(1) + " ℉");
+        $(".humidity").append("Humidity: ", response.main.humidity + "%");
+        $(".windSpeed").append("Wind Speed: ", response.wind.speed + " MPH");
         
         
         var newToList = $("<ul>");
         //Inserts the new city to the top of the list
-        $(".list-group").prepend(newToList, city_name);
+        $(".list-group-item").prepend(newToList, city_name);
         // console.log(newToList); 
     
         // var newCity = response.name
         // newToList.attr("name", newCity);
-        
+
     });
+
     })
+
+
+})
