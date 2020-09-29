139fe2d03401e21ee7c15c3bfb51d5b21105951e
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-29 03:44:39 -0700

    added api for 5 day forecast

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ $(document).ready(function () {
     // var cityHum = $(".humidity").val();
     // var cityWind = $(".windSpeed").val();
     
+    
 
     console.log(city_name)
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&units=imperial&appid=37574b1d6986c3e876db8a1d6e2f05dc";
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&units=imperial&appid=37574b1d6986c3e876db8a1d6e2f05dc";
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -21,48 +22,80 @@ $(document).ready(function () {
         console.log("response: " , response);
 
 
-        $(".cityName").text(city_name + " (" + date + ") ");
-        $(".temp").text("Temperature: ", response.main.temp.toFixed(1) + " ℉");
-        $(".humidity").text("Humidity: ", response.main.humidity + "%");
-        $(".windSpeed").text("Wind Speed: ", response.wind.speed + " MPH");
+        $(".cityName").append(city_name + " (" + date + ") ");
+        $(".temp").append("Temperature: ", response.list[0].main.temp.toFixed(1) + " ℉");
+        $(".humidity").append("Humidity: ", response.list[0].main.humidity + "%");
+        $(".windSpeed").append("Wind Speed: ", response.list[0].wind.speed + " MPH");
         
         // var realTemp = response.main.temp = num.toFixed(1);
         var newToList = $("<ul>");
         //Inserts the new city to the top of the list
+
         $(".list-group-item").prepend(newToList, city_name);
         // console.log(newToList); 
 
+        
+        var forecast = response.list[0].weather[0].main
 
-        if (response.weather[0].main == "Clear" || response.weather[0].main == "Sunny") {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("fas fa-sun");
+
+        if (forecast == "Clear" || forecast == "Sunny") {
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("fas fa-sun");
         }
 
-        else if (response.weather[0].main == "Snow") {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("far fa-snowflake");
+        else if (forecast == "Snow") {
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("far fa-snowflake");
         }
 
-        else if (response.weather[0].main == "Windy") {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("fas fa-wind");
+        else if (forecast == "Windy") {
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("fas fa-wind");
         }
 
-        else if (response.weather[0].main == "Rain" || response.weather[0].main == "Stormy") {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("fas fa-umbrella");
+        else if (forecast == "Rain" || forecast == "Stormy") {
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("fas fa-umbrella");
         }
 
-        else if (response.weather[0].main == "Clouds" || response.weather[0].main == "Thunder" ) {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("fas fa-cloud");
+        else if (forecast == "Clouds" || forecast == "Thunder" ) {
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("fas fa-cloud");
         }
 
         else {
-            $("#weatherIcon").removeClass("fas fa-arrow-left").addClass("fas fa-meteor");
+            $("#weatherIcon").removeClass("fas fa-arrow-circle-left").addClass("fas fa-meteor");
         }
         
       
 
-        var coordLat = response.coord.lat
-        var coordLon = response.coord.lon
-        console.log(coordLat)
-        console.log(coordLon)
+        var coordLat = response.city.coord.lat
+        var coordLon = response.city.coord.lon
+        // console.log(coordLat)
+        // console.log(coordLon)
+
+        var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + coordLat + "&lon=" + coordLon + "&appid=37574b1d6986c3e876db8a1d6e2f05dc";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .then(function(response2) {
+            console.log("response: " , response2);
+
+            $("#uvIndex").append("UV Index: ", response2.value)
+
+            if (response2.value < 2) {
+                $("#uvIndex").removeClass("badge badge-transparent").addClass("badge badge-success");
+            }
+    
+            else if (response2.value > 2 && response2.value < 6) {
+                $("#uvIndex").removeClass("badge badge-transparent").addClass("badge badge-warning");
+            }
+    
+            else {
+                $("#uvIndex").removeClass("badge badge-transparent").addClass("badge badge-danger");
+            }
+
+        
+        
+        
+        });
+
     });
 
     })
